26db1a032432ecebff5ad028058f7d346cfe59cd
Santiago199723 <71418175+Santiago199723@users.noreply.github.com>
2023-11-30 21:59:05 -0300

    mega fluxo login

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,12 @@ if (localStorage.getItem("token") == null) {
 const userLogado = JSON.parse(localStorage.getItem("userLogado"));
 
 const logado = document.querySelector("#logado");
-logado.innerHTML = `Olá, ${userLogado.nome}`;
+logado.innerHTML = `<span style="font-family: 'Verdana', sans-serif; color: #4CAF50; font-size: 1.5em;">Olá,</span> ${userLogado.nome}`;
 
 function sair() {
   localStorage.removeItem("token");
   localStorage.removeItem("userLogado");
   window.location.href = "./assets/html/signin.html";
 }
+
+
